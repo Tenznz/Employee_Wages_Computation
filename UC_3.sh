2b3449c1f1cd0d58e710a4669224c0beb86a910a
Duk <dhugkar@gmail.com>
2021-10-06 15:21:30 +0530

    Added UC_3.sh

---
+++ UC_3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=12
+randomCheck=$((RANDOM%3))
+
+if [ $isFullTime -eq $randomCheck ]
+then
+     empHrs=8
+elif  [ $isPartTime -eq $randomCheck ]
+then 
+     empHrs=8
+else
+     empHrs=0
+fi
+
+salary=$(($empHrs*$empRatePerHr))
+echo "salary: $salary"
+
